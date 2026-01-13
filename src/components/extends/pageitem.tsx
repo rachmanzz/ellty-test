6dbb897a896fa05426992eddd365b2eaaebd11db
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-01-13 15:34:38 +0800

    refactor: replace CheckBox with CustomCheckBox and enhance event handling

--- src/components/extends/pageitem.tsx
+++ src/components/extends/pageitem.tsx
@@ -1,10 +1,6 @@
-
-// type pageItemsProps = {
-//     children: string
-//     check: boolean
-
 import { useEffect, useRef, useState } from "react";
-import CheckBox from "../ui/checkbox";
+import { STATE_FOCUSED, STATE_HOVER, STATE_NORMAL, STATE_PRESSED, type EVENT_STATES } from "../ui/checkbox/state/interfaces";
+import CustomCheckBox from "../ui/checkbox";
 
 type pageItemProps = {
     checked: boolean
@@ -16,7 +12,7 @@ export default function PageItem({checked, label, onCheck}:pageItemProps) {
     const ref = useRef<HTMLButtonElement>(null)
     const mountRef = useRef<boolean>(false)
 
-    const [hover, setHoverEnter] = useState(false)
+    const [eventState, setEventState] = useState<EVENT_STATES>(STATE_NORMAL)
 
     useEffect(() => {
         if (!mountRef.current) {
@@ -24,8 +20,37 @@ export default function PageItem({checked, label, onCheck}:pageItemProps) {
             function onClick() {
                 onCheck()
             }
-            function onLeave () { setHoverEnter(()=> false) }
-            function onEnter () { setHoverEnter(()=> true) }
+            let StateOnFocus = false;
+            let StateOnHover = false;
+            let StateOnPress = false;
+            function onEnter () {
+                StateOnHover = true; 
+                setEventState(StateOnPress? STATE_PRESSED : STATE_HOVER);
+             }
+            function onLeave () {
+                StateOnHover = false;
+                setEventState(StateOnFocus ? STATE_FOCUSED : STATE_NORMAL)
+             }
+            function onFocus () { 
+                StateOnFocus = true;
+                setEventState(StateOnPress? STATE_PRESSED : StateOnHover ? STATE_HOVER : STATE_FOCUSED)
+            }
+            function onFocusOut () {
+                StateOnFocus = false;
+                setEventState(e => e === STATE_FOCUSED ? STATE_NORMAL : e)
+            }
+            function onPressed() {
+                StateOnPress = true;
+                setEventState(STATE_PRESSED)
+            }
+            function onPressedOut() {
+                StateOnPress = false;
+                setEventState(StateOnHover ? STATE_HOVER : StateOnFocus ? STATE_FOCUSED : STATE_NORMAL)
+            }
+            ref.current?.addEventListener("focus", onFocus)
+            ref.current?.addEventListener("blur", onFocusOut)
+            ref.current?.addEventListener("pointerdown", onPressed)
+            ref.current?.addEventListener("pointerup", onPressedOut)
             ref.current?.addEventListener("click", onClick)
             ref.current?.addEventListener("mouseenter", onEnter)
             ref.current?.addEventListener("mouseleave", onLeave)
@@ -35,7 +60,7 @@ export default function PageItem({checked, label, onCheck}:pageItemProps) {
     return (
         <button className="page-item" ref={ref}>
             <div>{label}</div>
-            <CheckBox hover={hover} style={{ width: 35, height: 35 }} checked={checked} />
+            <CustomCheckBox state={eventState} style={{ width: 35, height: 35 }} checked={checked} />
         </button>
     )
 }
